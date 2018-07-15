8d12e22f446bb81f4a758bbedb084d59602f8a69
tarosaowaluk <37224472+tarosaowaluk@users.noreply.github.com>
2018-07-15 18:16:37 +0700

    Update hims.php

--- hims.php
+++ hims.php
@@ -3,7 +3,7 @@ date_default_timezone_set("Asia/Bangkok");
 $t = date("H");
 if ($t < "10") {
     echo "Have a good morning!";
-} elseif ($t < "20") {
+} elseif ($t < "17") {
     echo "Have a good day!";
 } else {
     echo "Have a good night!";
