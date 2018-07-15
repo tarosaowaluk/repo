1c9e0a271d32c24631e0dbda8149b2ef2fd1b3ef
tarosaowaluk <37224472+tarosaowaluk@users.noreply.github.com>
2018-07-15 18:11:58 +0700

    Update hims.php

--- hims.php
+++ hims.php
@@ -4,7 +4,7 @@ $t = date("H");
 if ($t < "20") {
     echo "Have a good day!";
 if ($t == "18") {
-    echo "Have a good day!";
+    echo "Have a good day 18.00!";
 } else {
     echo "Have a good night!";
 }
