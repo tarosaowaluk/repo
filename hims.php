0d2b508f4c7e7ca39ee94d5efec5182c698da80f
tarosaowaluk <37224472+tarosaowaluk@users.noreply.github.com>
2018-07-15 18:14:05 +0700

    Update hims.php

--- hims.php
+++ hims.php
@@ -1,11 +1,12 @@
 <?php
 $t = date("H");
 
-if ($t == "20") {
+ if($t == "สวัสดีครับ") {
     echo "Have a good day!";
-if ($t == "18") {
+ if($t == "18") {
     echo "Have a good day 18.00!";
 } else {
     echo "Have a good night!";
 }
+         echo "$t";
 ?>
