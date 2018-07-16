dd64c1c85580a4b61720b96085da0a3eb41896b2
tarosaowaluk <37224472+tarosaowaluk@users.noreply.github.com>
2018-07-16 16:25:17 +0700

    Update DJ.php

--- DJ.php
+++ DJ.php
@@ -5,8 +5,8 @@ $sc_port = '7208';
 $sc_sid = 1; 
 
 $xml = @simplexml_load_file('http://' . $sc_ip . ':' . $sc_port . '/stats?sid=' . $sc_sid); 
-
-if ($xml === FALSE) { 
+$check = $current_title = $xml->STREAMSTATUS;
+if ($check == 0) { 
     $count_current = 'ดีเจออฟไลน์'; 
     $status = '0'; 
     $current_title = 'no title available'; 
