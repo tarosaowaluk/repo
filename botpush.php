2e8d0027e02946d4dfcfa794d0c1c21f313b887e
tarosaowaluk <37224472+tarosaowaluk@users.noreply.github.com>
2018-07-16 11:31:16 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -287,30 +287,6 @@
         replyMsg($arrayHeader,$arrayPostData);
     }
     else if($message == "FCมะปวย"){
-        $image_url = "https://www.picz.in.th/images/2018/07/16/NstwQb.gif";
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "image";
-        $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
-        $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
-        replyMsg($arrayHeader,$arrayPostData);
-    }
-    else if($message == "FCมะปวย2"){
-        $image_url = "https://www.picz.in.th/images/2018/07/16/Nst1rf.gif";
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "image";
-        $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
-        $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
-        replyMsg($arrayHeader,$arrayPostData);
-    }
-    else if($message == "FCมะปวย3"){
-        $image_url = "https://www.picz.in.th/images/2018/07/16/Nst5NZ.gif";
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "image";
-        $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
-        $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
-        replyMsg($arrayHeader,$arrayPostData);
-    }
-    else if($message == "ป้ายไฟมะปวย"){
         $image_url = "https://www.picz.in.th/images/2018/07/16/NstOrP.jpg";
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "image";
@@ -318,7 +294,7 @@
         $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
         replyMsg($arrayHeader,$arrayPostData);
     }
-    else if($message == "ป้ายไฟมะปวย2"){
+    else if($message == "FCมะปวย2"){
         $image_url = "https://www.picz.in.th/images/2018/07/16/Nstj0t.jpg";
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "image";
