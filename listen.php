8b40b7c312dc83088b39fb76ad0ca0de7567028b
tarosaowaluk <37224472+tarosaowaluk@users.noreply.github.com>
2018-07-16 14:14:10 +0700

    Update listen.php

--- listen.php
+++ listen.php
@@ -7,14 +7,16 @@ $sc_sid = 1;
 $xml = @simplexml_load_file('http://' . $sc_ip . ':' . $sc_port . '/stats?sid=' . $sc_sid); 
 
 if ($xml === FALSE) { 
-    $count_current = '0'; 
+    $count_current = 'ดีเจออฟไลน์'; 
     $status = '0'; 
     $current_title = 'no title available'; 
+    echo 'ขณะนี้' . $count_current; 
+
 } else { 
     $count_current = $xml->CURRENTLISTENERS; # counting current listeners 
     $status = $xml->STREAMSTATUS; # stream status 
     $current_title = $xml->SONGTITLE; # current stream-title 
+    echo 'ขณะนี้มีคนฟังจำนวน : ' . $count_current. ' คน'; 
 } 
 
-echo 'ขณะนี้มีคนฟังจำนวน : ' . $count_current. ' คน'; 
 ?>
