bc5331d0544ed12b4570d61caee4639d4f30f72a
tarosaowaluk <37224472+tarosaowaluk@users.noreply.github.com>
2018-07-16 16:24:31 +0700

    Update gettitle.php

--- gettitle.php
+++ gettitle.php
@@ -5,8 +5,8 @@ $sc_port = '7208';
 $sc_sid = 1; 
 
 $xml = @simplexml_load_file('http://' . $sc_ip . ':' . $sc_port . '/stats?sid=' . $sc_sid); 
-
-if ($xml === FALSE) { 
+$check = $current_title = $xml->STREAMSTATUS;
+if ($check == 0) { 
     $current_title = 'ดีเจออฟไลน์'; 
     echo 'ขณะนี้' . $current_title; 
 
