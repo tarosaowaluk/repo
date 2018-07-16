200c2e56691616d4e0a16549c2e881754b85ca1e
tarosaowaluk <37224472+tarosaowaluk@users.noreply.github.com>
2018-07-16 14:19:00 +0700

    Update gettitle.php

--- gettitle.php
+++ gettitle.php
@@ -1,1 +1,18 @@
-<? $data = file("https://radio10.plathong.net/7208/currentsong?sid=1");echo $data[0];?>
+<?php 
+
+$sc_ip = 'radio10.plathong.net'; 
+$sc_port = '7208'; 
+$sc_sid = 1; 
+
+$xml = @simplexml_load_file('http://' . $sc_ip . ':' . $sc_port . '/stats?sid=' . $sc_sid); 
+
+if ($xml === FALSE) { 
+    $current_title = 'ดีเจออฟไลน์'; 
+    echo 'ขณะนี้' . $current_title; 
+
+} else { 
+    $current_title = $xml->SONGTITLE; # current stream-title 
+    echo 'กำลังเล่นเพลง : ' . $current_title; 
+} 
+
+?>
