069a6f9c26335185661cb52f0c7d377e62cb49bc
tarosaowaluk <37224472+tarosaowaluk@users.noreply.github.com>
2018-07-16 11:32:10 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -286,6 +286,14 @@
         $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
         replyMsg($arrayHeader,$arrayPostData);
     }
+    else if($message == "QR"){
+        $image_url = "https://www.picz.in.th/images/2018/07/16/Nstn8S.jpg";
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "image";
+        $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
+        $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
+        replyMsg($arrayHeader,$arrayPostData);
+    }
     else if($message == "FCมะปวย"){
         $image_url = "https://www.picz.in.th/images/2018/07/16/NstOrP.jpg";
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
